cbca1c5ba2cb80a43010d4cb74de261f3ee38957
konboz <50484772+konboz@users.noreply.github.com>
2019-05-12 16:23:59 +0300

    Second commit

---
+++ FirstTask_BankAccount/Account.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstTask_BankAccount
+{
+    public class Account
+    {
+        const double depositLimit = 5000;
+        
+        string owner;
+        double balance;
+        int numberOfTransactions;
+
+        public bool Deposit(double amount)
+        {
+            if (amount <= depositLimit)
+            {
+                balance += amount;
+                numberOfTransactions++;
+                return true;
+            }
+            else
+                return false;
+
+        }
+
+        public bool Withdraw(double amount)
+        {
+            if (balance >= amount)
+            {
+                balance -= amount;
+                numberOfTransactions++;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public Account()
+        {
+
+        }
+
+        public Account(string owner, double balance, int numberOfTransactions)
+        {
+            this.owner = owner;
+            this.balance = balance;
+            this.numberOfTransactions = numberOfTransactions;
+        }
+        public override string ToString()
+        {
+            return "Account owner: " + owner + " Balance: " + balance + " Number of transactions: " + numberOfTransactions;
+        }
+    }
+}
+
---
+++ FirstTask_BankAccount/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FirstTask_BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account customer = new Account("Kostas", 452, 1);
+            Console.WriteLine(customer);
+            bool success = customer.Withdraw(200);
+            if (success)
+                Console.WriteLine("Withdrawal successful!");
+            else
+                Console.WriteLine("Withdrawal failed, unsufficient funds!");
+
+            Console.WriteLine(customer);
+
+            success = customer.Deposit(5800);
+            if (success)
+                Console.WriteLine("Deposit successful!");
+            else
+                Console.WriteLine("Deposit failed");
+            Console.WriteLine(customer);
+
+            success = customer.Withdraw(8200);
+            if (success)
+                Console.WriteLine("Withdrawal successful!");
+            else
+                Console.WriteLine("Withdrawal failed, unsufficient funds!");
+            Console.WriteLine(customer);
+
+            Console.ReadLine();
+        }
+    }
+}
